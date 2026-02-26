470cd0f6a4220d36dc8a286a74bdd3c0f51fc682
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-02-26 20:40:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Accordian from "./components/accordian";
+import LoadMoreData from "./components/load-more-data";
+import RandomColor from "./components/randomColorChnager";
+import StarRating from "./components/starRating";
+import ImageSlider from "./ImageSlider";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Accordian component */}
+      {/* <Accordian/> */}
+      {/* RandomColorChanger componenet */}
+      {/* <RandomColor /> */}
+      {/* star rating component */}
+      {/* <StarRating noOfStars={10}/> */}
+      {/* imageSlider component*/}
+      {/* <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      /> */}
+      {/* load-more-data component */}
+      <LoadMoreData />
+      
     </div>
   );
 }
---
+++ src/components/accordian/data.js
@@ -0,0 +1,28 @@
+const accordionData = [
+  {
+    id: 1,
+    question: "What is React?",
+    answer:
+      "React is a JavaScript library used to build user interfaces, especially for single-page applications. It allows developers to create reusable UI components.",
+  },
+  {
+    id: 2,
+    question: "What are components in React?",
+    answer:
+      "Components are reusable pieces of UI in React. They can be functional or class-based and help organize code into smaller, manageable parts.",
+  },
+  {
+    id: 3,
+    question: "What is useState hook?",
+    answer:
+      "useState is a React hook that allows you to add state to functional components. It helps you manage dynamic data inside your component.",
+  },
+  {
+    id: 4,
+    question: "What is useEffect hook?",
+    answer:
+      "useEffect is used to handle side effects in React such as API calls, subscriptions, or updating the DOM after rendering.",
+  },
+];
+
+export default accordionData;
